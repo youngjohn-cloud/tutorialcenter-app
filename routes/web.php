30c5fce0bf533fb1934cbbf2f1441ee0d1690b4a
Olugbenga Raymond <olugbengaraymond20@gmail.com>
2025-05-12 19:50:05 +0100

    Creating a redirection to the frontend

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->away('https://tutorialcenter.vercel.app/');
 });
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
