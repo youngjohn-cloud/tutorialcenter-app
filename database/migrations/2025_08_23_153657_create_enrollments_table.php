aacc5626c6ad29edd3990d1fd5b479bb71f5b715
Promzy004 <promiseedwin242@gmail.com>
2025-08-23 16:41:25 +0100

    enrollment and payments migration created and structured

---
+++ database/migrations/2025_08_23_153657_create_enrollments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('enrollments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('student_id');
+            $table->unsignedBigInteger('course_id');
+            $table->dateTime('end_date')->nullable();
+
+            // Foreign Keys
+            $table->foreign('student_id')->references('id')->on('students')->onDelete('cascade');
+            $table->foreign('course_id')->references('id')->on('courses')->onDelete('cascade');
+
+            $table->softDeletes(); // deleted_at
+            $table->timestamps(); // created_at, updated_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('enrollments');
+    }
+};
